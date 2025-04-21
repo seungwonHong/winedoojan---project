fbef4eb3f5d6240930ec454295d72fdcd4f011ad
numi8462 <yhk8462@naver.com>
2025-04-21 22:55:06 +0900

    feat: tailwind 설정 및 기본 폴더 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '**', // Allow all hostnames
+      },
+      {
+        protocol: 'https',
+        hostname: '**', // Allow all hostnames
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        wine: '#722F37',
+        claret: '#7F1734',
+        maroon: '#800000',
+        burgundy: '#800020',
+        malbec: '#9B0047',
+        merlot: '#7F171F',
+        winedregs: '#673147',
+        cabernet: '#8E5B6B',
+        ruby: '#E0115F',
+        garnet: '#830E0D',
+        beaujolais: '#80304C',
+        palepink: '#FADADD',
+        bordeaux: '#4C0013',
+        sangria: '#92000A',
+        shiraz: '#802539',
+        salmon: '#FA8072',
+        syrah: '#6A282C',
+        pinotnoir: '#653642',
+      },
+    },
+  },
+  plugins: [],
+};
