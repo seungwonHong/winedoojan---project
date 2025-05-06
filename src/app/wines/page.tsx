98e85ce57f8861b7801bdf68c9530e643423c936
seungwonHong <nonamed814@gmail.com>
2025-05-06 23:54:53 +0900

    edit

--- src/app/wines/page.tsx
+++ src/app/wines/page.tsx
@@ -79,7 +79,16 @@ const Wines = () => {
             {allWines !== null && allWines?.length > 0 ? (
               <>
                 {allWines.map((allwine) => (
-                  <WineCardBig key={allwine.id} wine={allwine} />
+                  <WineCardBig
+                    key={allwine.id}
+                    wine={{
+                      ...allwine,
+                      type: allwine.type.toUpperCase() as
+                        | "RED"
+                        | "WHITE"
+                        | "SPARKLING",
+                    }}
+                  />
                 ))}
                 {loading && <LoadingAnimation />}
               </>
